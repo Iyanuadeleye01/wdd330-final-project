7a2bb787529874c76ecb80ca0e55c3a14fe0aadd
adele <adeley1106@gmail.com>
2026-02-19 08:51:25 +0100

    Add property page is added

--- scripts/PropertyService.js
+++ scripts/PropertyService.js
@@ -4,7 +4,7 @@ import Property from "./Property.js";
 export default class PropertyService {
 
     static async fetchProperties(){
-        const response = await fetch("data/properties.json");
+        const response = await fetch("../data/properties.json");
         const data = await response.json();
         return data.map(item => new Property(item));
     };
